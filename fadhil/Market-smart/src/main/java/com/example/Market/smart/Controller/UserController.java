313217a36465c9faea792cc467903b3469db6bb6
Ashmally <hapsaahmada@gmail.com>
2025-07-05 18:07:23 +0300

    Create UserController.java

---
+++ fadhil/Market-smart/src/main/java/com/example/Market/smart/Controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.Market.smart.Controller;
+
+import com.example.Market.smart.Model.User;
+import com.example.Market.smart.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Registration endpoint
+    @PostMapping("/register")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
